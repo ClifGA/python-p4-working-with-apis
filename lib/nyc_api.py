617804280414a87f9d56ba07f0e2821d71708e19
Clifton Gockul <clifga@gmail.com>
2023-05-23 13:36:24 -0400

    completed lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,7 +9,19 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+
+    program_list =[]
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      program_list.append(program['agency'])
+
+    return program_list
 
 
 programs = GetPrograms().get_programs()
+programs_schools = GetPrograms().program_school()
 print(programs)
+for school in set(programs_schools):
+    print(school)
